57ce127b060108e8f9e26d829ab91e19bb9a6142
Kevin English <kenglish_hi@yahoo.com>
2009-03-22 19:17:38 -1000

    Moved format_db to after_save

--- app/models/fasta_file.rb
+++ app/models/fasta_file.rb
@@ -1,4 +1,25 @@
 class FastaFile < ActiveRecord::Base
-   has_attached_file :fasta , :styles => { :fastadb => { :quality => :better } },
-             :processors => [:formatdb] 
+  has_attached_file :fasta # , :styles => { :fastadb => { :quality => :better } },
+    # :processors => [:formatdb]
+
+  before_validation :set_label
+  validates_uniqueness_of :label
+  after_save :run_formatdb
+  
+  def set_label
+    if self.label.blank?
+      if fasta_file_name.match(/\.fasta$/)
+        self.label= fasta_file_name.sub(/\.fasta$/,'')
+        logger.error("[kenglish] setting lable  #{self.label}")
+      end
+    end
+  end
+
+  def run_formatdb
+    if fasta and File.exists?(fasta.path)
+      args = " -i #{fasta.path} -p F -o F -n #{fasta.path} " 
+      puts "[kenglish] HELLO KEVIN  #{fasta.path} args = #{args}" 
+      Paperclip.run "formatdb",  args
+    end
+  end
 end
--- test/unit/fasta_file_test.rb
+++ test/unit/fasta_file_test.rb
@@ -1,6 +1,8 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class FastaFileTest < ActiveSupport::TestCase
+  should_require_unique_attributes :label 
+
   # Replace this with your real tests.
   test "the truth" do
     assert true
