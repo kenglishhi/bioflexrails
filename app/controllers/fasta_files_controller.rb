b107e0f7b1f8901b3c2c25fef35f93efb4ad64b3
Kevin English <kenglish_hi@yahoo.com>
2009-03-26 19:36:06 -1000

    Fasta file controller updates...

--- app/controllers/fasta_files_controller.rb
+++ app/controllers/fasta_files_controller.rb
@@ -1,12 +1,15 @@
 class FastaFilesController < ApplicationController
   active_scaffold :fasta_files do |config| 
-    config.theme = :blue 
+   
+    config.list.label = "Fasta Files"
     config.list.columns = [:label, :fasta_file_name, :fasta_content_type, :fasta_file_size ]
     config.create.multipart = true
     config.create.columns = [:label, :fasta] 
     config.update.columns = [:label] 
+    config.show.columns = [:label, :fasta_file_name, :fasta_file_size,:created_at,:fasta_data]
     config.action_links.add "Blast", :parameters => {:controller => 'blast'},
                              :action => 'index', :type => :table, :page => true
+                             
   end
 
 end 
--- app/helpers/fasta_files_helper.rb
+++ app/helpers/fasta_files_helper.rb
@@ -1,5 +1,19 @@
 module FastaFilesHelper
   def fasta_form_column(record, input_name)
      file_field_tag input_name
-  end
+ end
+ 
+  def fasta_file_name_column(record)
+     link_to record.fasta_file_name, record.fasta.url
+ end
+ def fasta_data_column(record)
+     # Example 2 - Pass file to block
+   output = ""
+   File.open(record.fasta.path, "r") do |infile|
+      while (line = infile.gets)
+          output << line
+     end
+   end
+   '<div id="fasta-data"><pre>' + output + '</pre></div>'
+ end
 end
