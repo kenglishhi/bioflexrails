2245a24b5ed8019bbee53fe3593b9f8e30337f71
Kevin English <kenglish_hi@yahoo.com>
2009-03-26 19:30:53 -1000

    Fixed path in test file

--- test/unit/biosequence_test.rb
+++ test/unit/biosequence_test.rb
@@ -1,8 +1,15 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class BiosequenceTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   def test_truth
     assert true
+  end
+  test "load fasta file" do
+    old_count = Biosequence.count 
+    file = File.dirname(__FILE__) "/../fixtures/files/EST_Clade_A_5.fasta" 
+    Biosequence.load_fasta file
+    assert_equal Biosequence.count, old_count + 5, "Should load five sequences into the db" 
+     
   end
 end
