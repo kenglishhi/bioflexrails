f391f8d71eb09fe7f1b8153a6fb1e27c7933b2ff
Kevin English <kenglish_hi@yahoo.com>
2009-04-04 18:11:29 -1000

    Made extract sequences an asynchronous javascript request

--- app/helpers/fasta_files_helper.rb
+++ app/helpers/fasta_files_helper.rb
@@ -6,11 +6,11 @@ module FastaFilesHelper
   def fasta_file_name_column(record)
      link_to record.fasta_file_name, record.fasta.url
  end
- def biodatabase_column(record)
+ def biodatabase_extract_column(record)
    if record.biodatabase
-     link_to "Extracted"
-   else 
-     link_to "Extract Sequences", :action => 'extract_sequences', :id => record.id
+     "Extracted"
+   else
+      render  :partial => "/fasta_files/biodatabase_extract_link", :locals => {:record => record}
    end
  end
  
@@ -30,5 +30,23 @@ module FastaFilesHelper
     "ALOHA"
  end
  
+##   link_to_remote_with_spinner 'Hi', :url => hi_path, :container_id => 'container', :spinner => 'spinner' 
+# def link_to_remote_with_spinner(title, options)
+#  element_id = options.delete(:id) || ('link_to_' + title.underscore.tr(' ', '_'))
+#  container_id = options.delete(:container_id) || element_id
+##  
+#  returning '' do |out|
+##    unless spinner = options.delete(:spinner)
+##      spinner = "#{element_id}_spinner"
+##      out << image_tag('spinner.gif', :id => spinner, :style => 'display:none')
+##    end
+##    options[:complete] = "$('#{spinner}').hide(); " + (options[:complete] || "$('#{container_id}').show()")
+##    
+##    out << link_to_remote(title, { :loading => "$('#{container_id}').hide(); $('#{spinner}').show()" }.merge(options),
+##                                 { :id => element_id })
+##  end
+#end
+
+
  
 end
