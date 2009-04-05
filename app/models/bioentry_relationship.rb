8ba49803b0e721ce902e7c6f4d974e6c1c714c5b
Kevin English <kenglish_hi@yahoo.com>
2009-04-04 18:12:17 -1000

    Changed extract_sequences to use fasta file object

--- app/models/bioentry_relationship.rb
+++ app/models/bioentry_relationship.rb
@@ -2,5 +2,4 @@ class BioentryRelationship < ActiveRecord::Base
   belongs_to :subject_bioentry,    :class_name => 'Bioentry', :foreign_key => 'subject_bioentry_id'
   belongs_to :object_bioentry, :class_name => 'Bioentry', :foreign_key => 'object_bioentry_id'
   belongs_to :term
-
 end
