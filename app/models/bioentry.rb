1b1237ca1aa254b464cdc7ad5d397f69ca8c9b49
Kevin English <kenglish_hi@yahoo.com>
2009-04-06 21:19:40 -1000

    BIOentry

--- app/models/bioentry.rb
+++ app/models/bioentry.rb
@@ -1,5 +1,6 @@
 class Bioentry < ActiveRecord::Base
 
+
   #set
   belongs_to :biodatabase #, :class_name => "Biodatabase",:foreign_key =>"biodatabase_id"
   belongs_to :taxon #, :class_name => "Taxon",:foreign_key =>"taxon_id"
