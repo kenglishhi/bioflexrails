ead14e9fa1aa2fcedd65d255b71610a25219e0ef
Kevin English <kenglish_hi@yahoo.com>
2009-04-16 00:17:13 -1000

    Added check for database name

--- app/helpers/biosequences_helper.rb
+++ app/helpers/biosequences_helper.rb
@@ -4,6 +4,9 @@ module BiosequencesHelper
      image_tag(url_for({:action=>'to_image',:id=>feature_obj}))
   end
   def database_name_column(record)
-    record.bioentry.biodatabase.name
+    record.bioentry ? record.bioentry.biodatabase.name : "YO"
+  end
+  def name_column(record)
+    record.bioentry ? record.name: "No Bio Entry"
   end
 end
