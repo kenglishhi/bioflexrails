2964f9375d1fb21f198877939f25f10c4eff0ee5
Kevin English <kenglish_hi@yahoo.com>
2009-04-16 00:13:48 -1000

    Changed extract_sequences routine, now done in biodatabase after biodatabase is linked to fasta_file

--- app/models/fasta_file.rb
+++ app/models/fasta_file.rb
@@ -1,28 +1,43 @@
 class FastaFile < ActiveRecord::Base
   has_attached_file :fasta # , :styles => { :fastadb => { :quality => :better } },
     # :processors => [:formatdb]
-  belongs_to :biodatabase
+  belongs_to :biodatabase , :dependent => :destroy
   before_validation :set_label
   validates_uniqueness_of :label
   before_destroy :remove_fasta_dbs
-  
+
+  def is_generated?
+    # looks prettier
+    is_generated
+  end
   def set_label
     if self.label.blank?
       if fasta_file_name && fasta_file_name.match(/\.fasta$/)
-        self.label= fasta_file_name.sub(/\.fasta$/,'')
+        self.label = fasta_file_name.sub(/\.fasta$/,'')
         logger.error("[kenglish] setting label  #{self.label}")
       end
     end
   end
   
   def extract_sequences
-    if self.fasta  # and File.exists?(fasta.path)
-      Bioentry.load_fasta(self)
-      self.biodatabase = Biodatabase.find_by_name(self.label)
-      self.save
+    if fasta  # and File.exists?(fasta.path)
+      unless biodatabase
+        self.biodatabase = Biodatabase.find_by_name(File.basename(label)  ) || Biodatabase.create(:name => File.basename(label),:fasta_file => self  )
+        logger.error("[kenglish] fasta_file.biodatabase_id = #{biodatabase.id}")
+        save
+        unless self.biodatabase.fasta_file == self
+          self.biodatabase.fasta_file = self
+          self.biodatabase.save
+        end
+
+        logger.error("[kenglish] new biodatabase.fasta_file.id = #{self.biodatabase.fasta_file.inspect}")
+        
+      end
+     self.biodatabase.load_fasta
     end
 #    Bioentry.load_fasta fasta.path
   end
+  
   def formatdb
     puts "called formatdb fasta = #{fasta} File.exists?(fasta.path) = #{File.exists?(fasta.path)} " 
     if fasta and File.exists?(fasta.path)
